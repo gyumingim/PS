149e9a6445c696b3541204a9b9edd817b33163ca
gyumingim <a31620504@gmail.com>
2025-07-26 17:52:00 +0900

    단어수학 불명예 풀이

--- 1339단어수학.py
+++ 1339단어수학.py
@@ -5,51 +5,34 @@
 
 큰 자리수에 큰 수를 대입하는건 안되는 방법임, 모든 경우의 수?
 
-글자수 만큼 모든 경우의 수를 대입하는 방법?
+글자수 만큼 모든 경우의 수를 대입하는
+
+ㄴㄴ
+
+알파벳마다 자릿수를 전부 더해서
+높은만큼 9,8,7 을 할당하면 된다
+
+수치
 '''
-from copy import deepcopy
-
-try:
-
-    N = int(input())
-    lst = []
-
-    dic = {chr(c): -1 for c in range(ord("A"), ord("Z") + 1)}  # A~Z 모두 -1로 초기화
+N = int(input())
+word_list = [input().strip() for _ in range(N)]
+alphabet = {}
 
 
-    for _ in range(N):
-        lst.append(input())
-
-    temp_lst = deepcopy(lst)
-
-    i = 9
-    while i > 0:
-        lst = sorted(lst, key=len, reverse=True)
-        
-        if "".join(lst) == "":
-            break
-
-        if dic[lst[0][0]] == -1:
-            dic[lst[0][0]] = i
-            lst[0] = lst[0][1:]
-            i -= 1
+for word in word_list:
+    digit = len(word) - 1
+    for w in word:
+        if w in alphabet:
+            alphabet[w] += 10**digit
         else:
-            lst[0] = lst[0][1:]
+            alphabet[w] = 10**digit
+        digit -= 1
 
 
-    sum_ = 0
-
-    for l in temp_lst:
-        num = ""
-        for letter in l:
-            num += str(dic[letter])
-        
-        sum_ += int(num)
-
-    print(sum_)
-
-        
-        
-
-except Exception:
-    print("dd")
+words_sort = sorted(alphabet.values(), reverse=True) 
+result = 0
+num = 9
+for k in words_sort:
+    result += k * num 
+    num -= 1
+print(result)
